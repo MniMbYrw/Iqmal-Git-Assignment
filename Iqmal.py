ef2ccf8a1e52a09d8db2c04c2ef95d39b23c1b1e
iqmal <iqmalqwerty@gmail.com>
2023-04-08 13:12:18 -0700

    area_hl swap parameter inputs to match name

--- Iqmal.py
+++ Iqmal.py
@@ -7,7 +7,7 @@ from Emily import rect_area
 def rect_surface_area(length, width, height):
     area_lw = rect_area(length, width)
     area_wh = rect_area(width, height)
-    area_hl = rect_area(length,height)
+    area_hl = rect_area(height, length)
     # for test
     # print(area_lw, area_wh, area_hl)
     return 2 * (area_lw + area_wh + area_hl)
