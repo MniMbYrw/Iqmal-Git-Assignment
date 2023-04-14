4e7e241ab4eb20899bcbb963dd928b703352edde
iqmal <iqmalqwerty@gmail.com>
2023-04-14 10:19:14 -0700

    change pie parameters

--- week2.py
+++ week2.py
@@ -1,12 +1,13 @@
 import matplotlib.pyplot as plot
 
 # set up your lists
-numlist = [8, 6, 5, 3]
-namelist = ['freshmen', 'sophomores', 'juniors', 'seniors']
-colorlist = ['red', 'green', 'pink', 'yellow']
-explodelist = [0.1, 0.0, 0.0, 0.0]
+numlist = [8, 7, 4, 2]
+namelist = ['blue', 'orange', 'pink', 'yellow']
+colorlist = ['blue', 'orange', 'pink', 'yellow']
+explodelist = [0.05, 0.0, 0.0, 0.0]
 # make the pie chart
-plot.pie(numlist, labels=namelist, autopct='%.2f%%', colors=colorlist, explode=explodelist, startangle=90)
+plot.pie(numlist, labels=namelist, autopct='%.1f%%', colors=colorlist, explode=explodelist,
+         startangle=120, shadow=True, wedgeprops={"edgecolor": 'black'})
 plot.axis('equal')
 plot.savefig('piechart.png')
 plot.show()
