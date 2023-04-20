cf9aa05a55ea7d1c844ef7f5f6892bbf993e637f
iqmal <iqmalqwerty@gmail.com>
2023-04-20 13:10:03 -0700

    initialize WFQ

---
+++ WFQ.py
@@ -0,0 +1,12 @@
+# WFQ assignment
+
+# Pseudocode
+# Print 3 Priority packets until until Priority queue is empty
+# Then print 2 Standard packets until standard queue is empty
+# Then print 1 Economy packet until economy queue is empty
+
+# space with file
+f = open("C:/Users/iqmal/Documents/School/S23/CS162/input queue.txt", 'r')
+lines = f.readlines()
+print(lines)
+f.close()
