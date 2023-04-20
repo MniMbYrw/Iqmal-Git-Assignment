9e70c44a4d1ca75562190fe51f06bd7a6303277f
iqmal <iqmalqwerty@gmail.com>
2023-04-20 11:02:19 -0700

    expand random value range in array

--- pyarrays.py
+++ pyarrays.py
@@ -3,7 +3,7 @@
 import numpy as np
 
 # make random 5x5 array and print it
-array = np.random.randint(0, 9, size=(5, 5), dtype=int)
+array = np.random.randint(0, 999, size=(5, 5), dtype=int)
 print(array)
 
 # print second row third column
