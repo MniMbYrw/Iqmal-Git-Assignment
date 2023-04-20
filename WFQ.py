80247a54ae4bb0bcf901bbe07147951c29ff7313
iqmal <iqmalqwerty@gmail.com>
2023-04-20 13:34:04 -0700

    initialize priority queues and fill them

--- WFQ.py
+++ WFQ.py
@@ -9,4 +9,19 @@
 f = open("C:/Users/iqmal/Documents/School/S23/CS162/input queue.txt", 'r')
 lines = f.readlines()
 print(lines)
+# initialize priority queues
+premium = []
+standard = []
+economy = []
+# fill priority queues
+for x in lines:
+    if x[0] == 'P':
+        # remove class marker as queue is now class marker, also do extra characters at same time
+        premium.append(x.strip('P \n'))
+    elif x[0] == 'S':
+        standard.append(x.strip('S \n'))
+    else:
+        economy.append(x.strip('E \n'))
+print(premium, standard, economy)
+
 f.close()
